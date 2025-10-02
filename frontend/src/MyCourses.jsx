d1b0019a14ab9d288e493e204f7e6bcda51e715a
Brian Ren <brianren@buffalo.edu>
2025-10-01 20:51:01 -0400

    Implemented Router and created search bar
    The router allows for navigation/redirection between web pages within the application. A simple frontend for the search bar was created on a new page, My Courses.

---
+++ frontend/src/MyCourses.jsx
@@ -0,0 +1,24 @@
+export default function MyCourses() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",   // centers horizontally
+        textAlign: "center",
+        paddingTop: "2rem",     // spacing from the very top
+      }}
+    >
+      {/* Page Title */}
+      <h1>My Courses</h1>
+
+      {/* Search Bar */}
+      <div style={{ marginTop: "1rem" }}>
+        <input
+          type="text"
+          placeholder="Search by course code, name, or professor..."
+        />
+      </div>
+    </div>
+  );
+}
